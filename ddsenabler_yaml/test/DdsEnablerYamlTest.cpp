acc3140839c605394af175c1f76343036e571b7e
adriancampo <adriancampo@eprosima.com>
2024-10-14 10:41:39 +0200

    Fixed cofigFile tests.
    Signed-off-by: adriancampo <adriancampo@eprosima.com>

--- ddsenabler_yaml/test/DdsEnablerYamlTest.cpp
+++ ddsenabler_yaml/test/DdsEnablerYamlTest.cpp
@@ -24,47 +24,6 @@
 using namespace eprosima;
 using namespace eprosima::ddsenabler::yaml;
 
-// TEST(DdsEnablerYamlTest, get_ddsenabler_correct_configuration_json)
-// {
-//     const char* json_str =
-//             "{"
-//             "  \"dds\": {"
-//             "    \"domain\": 0"
-//             "  },"
-//             "  \"ddsenabler\": null,"
-//             "  \"specs\": {"
-//             "    \"threads\": 12,"
-//             "    \"logging\": {"
-//             "      \"verbosity\": \"info\","
-//             "      \"filter\": {"
-//             "        \"error\": \"DDSENABLER_ERROR\","
-//             "        \"warning\": \"DDSENABLER_WARNING\","
-//             "        \"info\": \"DDSENABLER_INFO\""
-//             "      }"
-//             "    }"
-//             "  }"
-//             "}";
-
-//     nlohmann::json json = nlohmann::json::parse(json_str);
-
-//     // Load configuration from JSON
-//     EnablerConfiguration configuration(true, json);
-
-//     utils::Formatter error_msg;
-
-//     ASSERT_TRUE(configuration.is_valid(error_msg));
-//     ASSERT_EQ(configuration.n_threads, 12);
-
-//     ASSERT_TRUE(configuration.ddspipe_configuration.log_configuration.is_valid(error_msg));
-//     ASSERT_EQ(configuration.ddspipe_configuration.log_configuration.verbosity.get_value(), utils::VerbosityKind::Info);
-//     ASSERT_EQ(configuration.ddspipe_configuration.log_configuration.filter[utils::VerbosityKind::Error].get_value(),
-//             "DDSENABLER_ERROR");
-//     ASSERT_EQ(configuration.ddspipe_configuration.log_configuration.filter[utils::VerbosityKind::Warning].get_value(),
-//             "DDSENABLER_WARNING");
-//     ASSERT_EQ(configuration.ddspipe_configuration.log_configuration.filter[utils::VerbosityKind::Info].get_value(),
-//             "DDSENABLER_INFO");
-// }
-
 TEST(DdsEnablerYamlTest, get_ddsenabler_correct_configuration_yaml)
 {
     const char* yml_str =
@@ -139,7 +98,6 @@ TEST(DdsEnablerYamlTest, get_ddsenabler_incorrect_n_threads_configuration_yaml)
     EXPECT_THROW({EnablerConfiguration configuration(yml);}, std::exception);
 }
 
-
 TEST(DdsEnablerYamlTest, get_ddsenabler_default_values_configuration_yaml)
 {
     const char* yml_str =
@@ -157,11 +115,18 @@ TEST(DdsEnablerYamlTest, get_ddsenabler_default_values_configuration_yaml)
     ASSERT_EQ(configuration.n_threads, DEFAULT_N_THREADS);
 }
 
+TEST(DdsEnablerYamlTest, get_ddsenabler_incorrect_path_configuration_json)
+{
+    const char* path_str = "incorrect/path/file";
+
+    EXPECT_THROW({EnablerConfiguration configuration(path_str, true);}, eprosima::utils::ConfigurationException);
+}
+
 TEST(DdsEnablerYamlTest, get_ddsenabler_incorrect_path_configuration_yaml)
 {
     const char* path_str = "incorrect/path/file";
 
-    EXPECT_THROW({EnablerConfiguration configuration(path_str);}, eprosima::utils::ConfigurationException);
+    EXPECT_THROW({EnablerConfiguration configuration(path_str, false);}, eprosima::utils::ConfigurationException);
 }
 
 int main(
